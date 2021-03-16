6062271b57617b96804e8e7885bc61fb8c42d595
Jordan <jpribanic88@icloud.com>
2021-03-16 15:43:59 -0400

    Added draw_bars() function
    Added ability to draw the bars on the screen that will be used to visualize the sorting. Also added a few new color constant variables to use when drawing itmes to the screen

--- main.py
+++ main.py
@@ -9,11 +9,31 @@ WIDTH, HEIGHT = 1100, 700
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sorting Algorithm Visualizer")
 
+# Color variables
+WHITE = (250, 250, 250)
+GREY = (100, 100, 100)
+RED = (255, 50, 50)
+BLUE = (50, 50, 255)
+
 # Frames per second
 FPS = 30
 
-# Color variables
-WHITE = (250, 250, 250)
+# List of bar heights
+BAR_HEIGHTS = [num for num in range(1, 501)]
+
+# Bar dimension and location constants
+BAR_WIDTH = 2
+BAR_FROM_TOP = 550 
+BAR_FROM_LEFT = 50 
+
+
+# Draw bars at random before being sorted
+def draw_bars():
+    for i in range(len(BAR_HEIGHTS)):
+        pygame.draw.rect(WIN, GREY, pygame.Rect(BAR_FROM_LEFT + BAR_WIDTH * i, BAR_FROM_TOP - BAR_HEIGHTS[i], BAR_WIDTH, BAR_HEIGHTS[i]))
+
+    pygame.display.update()
+
 
 # Draw and update window
 def draw_window():
@@ -33,6 +53,7 @@ def main():
                 run = False
                 
         draw_window()
+        draw_bars()
     
     pygame.quit()
 
